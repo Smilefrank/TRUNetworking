8bf541be795a0ce6dc38a255cff6e2794913ae09
wangbf <wangbf@jfinfo.com>
2021-07-09 11:05:54 +0800

    子组件TRUNetWorking的初始化创建

---
+++ TRUNetworking/Classes/BFNetworking.h
@@ -0,0 +1,16 @@
+//
+//  BFNetworking.h
+//  TRUNetworking
+//
+//  Created by 丰华财经 on 2021/7/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BFNetworking : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/BFNetworking.m
@@ -0,0 +1,12 @@
+//
+//  BFNetworking.m
+//  TRUNetworking
+//
+//  Created by 丰华财经 on 2021/7/8.
+//
+
+#import "BFNetworking.h"
+
+@implementation BFNetworking
+
+@end
